7420bdc34415ac8ddfd229258fdc0a2dd731a277
milletpu <milletpu@gmail.com>
2016-11-28 01:10:05 +0800

    文本与其对应的极性

--- DM-hw/emotion_data_processing.py
+++ DM-hw/emotion_data_processing.py
@@ -1,18 +1,23 @@
 # -*- encoding: utf8 -*-
 import numpy as np
+import os
 import pandas as pd
 
 
-def get_major_content(url = '1.ann'):
-    file = open(url)
-    line_split = [ line.strip().split() for line in file.readlines()]
+def get_major_content(url):
+
+    if os.path.exists(url):
+        file = open(url)
+        line_split = [line.strip().split() for line in file.readlines()]
+        file.close()
+    else:
+        return
+
     T1_name = []  #存放正极性标识[ [T1/T2/T3],... ]
     T2_name = []  #存放负极性标识[ [T1/T2/T3],... ]
-
     for line in line_split:
         if line[1]=="Positive": T1_name.append([line[2],1]);
         if line[1]=="Negative": T2_name.append([line[2],-1]);
-    print T1_name
 
     content = [] #存放极性内容与极性[ [Claim,-1/1] ,...]
     for line in line_split:
@@ -20,12 +25,13 @@ def get_major_content(url = '1.ann'):
             if T1_name[idx][0] == line[0]:
                 content.append([line[4],T1_name[idx][1]])
         for idx in range(len(T2_name)):
-            if T1_name[idx][0] == line[0]:
+            if T2_name[idx][0] == line[0]:
                 content.append([line[4],T2_name[idx][1]])
     data = pd.DataFrame(content,columns = ["content","jixing"])
 
-    file.close()
     return data
 
-if __name__ == "__main___":
-    print get_major_content('1.ann')
+if __name__ == "__main__":
+    i = 33
+    url = '%d.ann'%i
+    print get_major_content(url)
